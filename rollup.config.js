dffa809ec05a3af740b63cc04a4a69e61a4771a5
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2020-09-02 16:07:15 +0200

    Initial version

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import typescriptPlugin from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
+import typescript from "typescript";
+
+import pkg from "./package.json";
+
+export default {
+    input: "src/index.ts",
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+            exports: "named",
+            sourcemap: true,
+        },
+        {
+            file: pkg.module,
+            format: "es",
+            exports: "named",
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        external(),
+        resolve(),
+        typescriptPlugin({
+            clean: true,
+            typescript,
+            exclude: ["**.test.tsx", "**.stories.tsx"],
+        }),
+        commonjs(),
+    ],
+};
---
+++ src/react-client.ts
@@ -0,0 +1,112 @@
+import * as React from "react";
+import { OidcJwtClient, OidcJwtClientOptions, oidcJwtClient } from "./client";
+
+interface OidcJwtContextData {
+    client: OidcJwtClient;
+}
+
+const OidcJwtContext = React.createContext<OidcJwtContextData | null>(null);
+
+interface OidcJwtProviderProps {
+    client: OidcJwtClient | OidcJwtClientOptions;
+    shouldRequireLogin?: boolean;
+    shouldPerformLogin?: boolean;
+    shouldMonitorAccessTokens?: boolean;
+}
+
+function isClientOptions(
+    obj: OidcJwtClient | OidcJwtClientOptions
+): obj is OidcJwtClientOptions {
+    return "url" in obj;
+}
+
+export const OidcJwtProvider: React.FC<OidcJwtProviderProps> = (props) => {
+    const {
+        client: clientProp,
+        shouldRequireLogin = false,
+        shouldPerformLogin = false,
+        shouldMonitorAccessTokens = true,
+        children,
+    } = props;
+    const contextValue: OidcJwtContextData = React.useMemo(() => {
+        return {
+            client: isClientOptions(clientProp)
+                ? oidcJwtClient(clientProp)
+                : clientProp,
+        };
+    }, [clientProp]);
+    const { client } = contextValue;
+    React.useEffect(() => {
+        if (client.receiveSessionToken()) {
+            return;
+        }
+        if (client.haveSessionToken()) {
+            if (shouldMonitorAccessTokens) {
+                client.monitorAccessToken();
+            }
+        } else {
+            if (shouldPerformLogin) {
+                const params: Record<string, string> = {};
+                if (!shouldRequireLogin) {
+                    params.prompt = "none";
+                }
+                client.authorize(params);
+            }
+        }
+        return () => {
+            if (shouldMonitorAccessTokens) {
+                client.stopMonitoringAccessToken();
+            }
+        };
+    }, [client]);
+
+    return React.createElement(
+        OidcJwtContext.Provider,
+        { value: contextValue },
+        children
+    );
+};
+
+export function usePromiseResult<T>(
+    f: () => Promise<T> | null | undefined,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    deps: any[]
+): T | null {
+    const [value, setValue] = React.useState<T | null>(null);
+    React.useEffect(() => {
+        f()?.then((result) => {
+            setValue(result);
+        });
+    }, deps);
+    return value;
+}
+
+export function useOidcJwtClient(): OidcJwtClient | null {
+    return React.useContext(OidcJwtContext)?.client ?? null;
+}
+
+export function useAccessTokenClaims(): Record<string, unknown> | null {
+    const client = useOidcJwtClient();
+    return (
+        usePromiseResult(() => client?.getAccessToken(), [client])?.claims ??
+        null
+    );
+}
+
+export function useUserInfo(): Record<string, unknown> | null {
+    const client = useOidcJwtClient();
+    return usePromiseResult(() => client?.getUserInfo(), [client]) ?? null;
+}
+
+export function useAccessToken(): () => Promise<string | null> {
+    const client = useOidcJwtClient();
+    return React.useCallback(() => {
+        if (client) {
+            return client
+                ?.getAccessToken()
+                .then((result) => result?.token ?? null);
+        } else {
+            return Promise.resolve(null);
+        }
+    }, [client]);
+}
