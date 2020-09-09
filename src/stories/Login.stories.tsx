87259f548a9a457346c0c1bd2ac5987214402eb3
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2020-09-09 21:12:27 +0200

    Simplify storybook example

--- src/stories/Login.stories.tsx
+++ src/stories/Login.stories.tsx
@@ -15,47 +15,6 @@ interface TemplateProps {
     shouldMonitorAccessTokens: boolean;
 }
 
-const UserInfo = () => {
-    const info = useAuthUserInfo();
-    return <pre>{JSON.stringify(info, undefined, 4)}</pre>;
-};
-
-const AccessTokenClaims = () => {
-    const info = useAuthAccessClaims();
-    return <pre>{JSON.stringify(info, undefined, 4)}</pre>;
-};
-
-const Buttons = () => {
-    const [token, setToken] = useState<null | string>(null);
-    const claims = useAuthAccessClaims();
-    const { authorize, logout } = useAuthControls();
-    const fetchAccessToken = useAuthAccessToken();
-    const onClickFetchToken = React.useCallback(() => {
-        fetchAccessToken().then((token) => {
-            setToken(token);
-        });
-    }, [fetchAccessToken, setToken]);
-    const onClickLogout = React.useCallback(() => {
-        logout();
-    }, [logout]);
-    const onClickLogin = React.useCallback(() => {
-        authorize();
-    }, [authorize]);
-    return (
-        <div>
-            {claims && <button onClick={onClickLogout}>Log out</button>}
-            {claims && <button onClick={onClickFetchToken}>Fetch token</button>}
-            {!claims && <button onClick={onClickLogin}>Log in</button>}
-            {token && (
-                <>
-                    <h2>Token</h2>
-                    <textarea value={token}></textarea>
-                </>
-            )}
-        </div>
-    );
-};
-
 const Template: Story<TemplateProps> = (props: TemplateProps) => {
     const {
         url,
@@ -73,13 +32,49 @@ const Template: Story<TemplateProps> = (props: TemplateProps) => {
             shouldAttemptLogin={shouldAttemptLogin}
             shouldMonitorAccessTokens={shouldMonitorAccessTokens}
         >
-            <Buttons />
+            <Content />
+        </OidcJwtProvider>
+    );
+};
+
+const Content = () => {
+    const userInfo = useAuthUserInfo();
+    const [token, setToken] = useState<null | string>(null);
+    const claims = useAuthAccessClaims();
+    const { authorize, logout } = useAuthControls();
+    const fetchAccessToken = useAuthAccessToken();
+    const onClickFetchToken = React.useCallback(() => {
+        fetchAccessToken().then((token) => {
+            setToken(token);
+        });
+    }, [fetchAccessToken, setToken]);
+    const onClickLogout = React.useCallback(() => {
+        logout();
+    }, [logout]);
+    const onClickLogin = React.useCallback(() => {
+        authorize();
+    }, [authorize]);
+    return (
+        <>
+            <div>
+                {claims && <button onClick={onClickLogout}>Log out</button>}
+                {claims && (
+                    <button onClick={onClickFetchToken}>Fetch token</button>
+                )}
+                {!claims && <button onClick={onClickLogin}>Log in</button>}
+                {token && (
+                    <>
+                        <h2>Token</h2>
+                        <textarea value={token}></textarea>
+                    </>
+                )}
+            </div>
             <hr />
             <h1>User info</h1>
-            <UserInfo />
+            <pre>{JSON.stringify(userInfo, undefined, 4)}</pre>
             <h1>Access token claims</h1>
-            <AccessTokenClaims />
-        </OidcJwtProvider>
+            <pre>{JSON.stringify(claims, undefined, 4)}</pre>
+        </>
     );
 };
 
