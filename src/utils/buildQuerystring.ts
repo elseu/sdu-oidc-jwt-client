fbf4194234ddfcf464fe2e6aa418238fd1f14455
Daphne Smit <daphnesmit1986@gmail.com>
2021-06-29 10:19:33 +0200

    fix: remove buildQuerystring in favor of query-string package thats already included

--- src/utils/buildQuerystring.ts
+++
@@ -1,9 +0,0 @@
-import { Params } from '../store';
-
-function buildQuerystring(params: Params): string {
-  return Object.keys(params)
-    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-    .join('&');
-}
-
-export { buildQuerystring };
