fae42caa2b7248077d3acbd6518197df818f4f73
Daphne <daphnesmit1986@gmail.com>
2022-12-06 20:00:17 +0100

    Update src/store.ts
    Co-authored-by: Tim de Wolf <tpdewolf@gmail.com>

--- src/store.ts
+++ src/store.ts
@@ -181,7 +181,7 @@ const USER_INFO_TOKEN_STORAGE_KEY = 'oidc_jwt_provider_user_info';
 const RETRY_LOGIN_STORAGE_KEY = 'oidc_jwt_provider_retry_login';
 
 function createOidcJwtClientStore(
-  options?: OidcJwtClientOptions,
+  options: OidcJwtClientOptions | false,
   removeTokenFromUrlFunction?: (url: string) => void,
 ) {
   return create<UseOidcJwtClientStore>((set, get) => {
