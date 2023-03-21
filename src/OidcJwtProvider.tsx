a547b2160028e37372cfbf99fc64f3418e14f56a
Daphne Smit <daphnesmit1986@gmail.com>
2023-03-21 12:32:31 +0100

    fix: dont set zustand state on load

--- src/OidcJwtProvider.tsx
+++ src/OidcJwtProvider.tsx
@@ -1,11 +1,12 @@
 import React, { createContext, useContext, useEffect, useRef, useState } from 'react';
+import { useIsomorphicLayoutEffect } from 'react-use';
 import { StoreApi, UseBoundStore, useStore } from 'zustand';
 
 import {
   CSRF_TOKEN_STORAGE_KEY,
   LOGGED_IN_TOKEN_STORAGE_KEY,
   RETRY_LOGIN_STORAGE_KEY,
-  USER_INFO_TOKEN_STORAGE_KEY,
+  USER_INFO_TOKEN_STORAGE_KEY
 } from './constants';
 import { Storage } from './storage';
 import { createOidcJwtClientStore } from './store';
@@ -44,54 +45,54 @@ const OidcJwtInitializer: React.FC<
   isMounted = false,
   children,
 }) => {
-  const authService = useOidcJwtStore((state) => state.service);
-  const isLoggedIn = useOidcJwtStore((state) => state.authState.isLoggedIn);
-  const setState = useOidcJwtStore((state) => state.setState);
-  const [isInitializing, setIsInitializing] = useState(false);
+    const authService = useOidcJwtStore((state) => state.service);
+    const isLoggedIn = useOidcJwtStore((state) => state.authState.isLoggedIn);
+    const setState = useOidcJwtStore((state) => state.setState);
+    const [isInitializing, setIsInitializing] = useState(false);
 
-  useEffect(() => {
-    if (!isMounted) return;
+    useEffect(() => {
+      if (!isMounted) return;
 
-    authService?.loadInitialData().then(() => setState(authService.state));
-  }, [authService, isMounted, setState]);
+      authService?.loadInitialData().then(() => setState(authService.state));
+    }, [authService, isMounted, setState]);
 
-  useEffect(() => {
-    if (!isLoggedIn || !shouldMonitorAccessTokens || !isMounted) return;
+    useEffect(() => {
+      if (!isLoggedIn || !shouldMonitorAccessTokens || !isMounted) return;
 
-    authService?.monitorAccessToken(() => setState(authService.state));
+      authService?.monitorAccessToken(() => setState(authService.state));
 
-    return () => authService?.stopMonitoringAccessToken();
-  }, [isLoggedIn, authService, shouldMonitorAccessTokens, setState, isMounted]);
+      return () => authService?.stopMonitoringAccessToken();
+    }, [isLoggedIn, authService, shouldMonitorAccessTokens, setState, isMounted]);
 
-  useEffect(() => {
-    if (!authService || !isMounted) return;
+    useEffect(() => {
+      if (!authService || !isMounted) return;
 
-    const { csrfToken } = authService.getCsrfToken();
-    if (typeof window === 'undefined' || isLoggedIn || !shouldAttemptLogin || !!csrfToken) return;
+      const { csrfToken } = authService.getCsrfToken();
+      if (typeof window === 'undefined' || isLoggedIn || !shouldAttemptLogin || !!csrfToken) return;
 
-    authService.authorize({ prompt: 'none' });
-  }, [authService, isLoggedIn, isMounted, shouldAttemptLogin]);
+      authService.authorize({ prompt: 'none' });
+    }, [authService, isLoggedIn, isMounted, shouldAttemptLogin]);
 
-  useEffect(() => {
-    const isInitializing =
-      !authService?.getCsrfToken().csrfToken && shouldAttemptLogin && !isLoggedIn;
+    useEffect(() => {
+      const isInitializing =
+        !authService?.getCsrfToken().csrfToken && shouldAttemptLogin && !isLoggedIn;
 
-    setIsInitializing(isInitializing);
-  }, [authService, isLoggedIn, shouldAttemptLogin]);
+      setIsInitializing(isInitializing);
+    }, [authService, isLoggedIn, isMounted, shouldAttemptLogin]);
 
-  if (isInitializing) {
-    return null;
-  }
+    if (isInitializing) {
+      return null;
+    }
 
-  return <>{children}</>;
-};
+    return <>{children}</>;
+  };
 
 const OidcJwtProvider: React.FC<React.PropsWithChildren<OidcJwtProviderProps>> = (props) => {
   const { client, removeTokenFromUrlFunction = removeTokenFromUrl, children } = props;
   const [isMounted, setMounted] = useState(false);
   const store = useRef(createOidcJwtClientStore(client, removeTokenFromUrlFunction)).current;
 
-  useEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     const initialState: AuthState = {
       userInfo: Storage.get(USER_INFO_TOKEN_STORAGE_KEY),
       csrfToken: Storage.get(CSRF_TOKEN_STORAGE_KEY),
@@ -103,7 +104,6 @@ const OidcJwtProvider: React.FC<React.PropsWithChildren<OidcJwtProviderProps>> =
     const service = store.getState().service;
 
     service?.setState(initialState);
-    store.setState({ authState: initialState });
 
     setMounted(true);
   }, [store]);
